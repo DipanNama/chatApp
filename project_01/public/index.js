770de7ae58501b5b2f18d07d9fb9d357a9cc3bb0
Dipan Nama <dipannama91@gmail.com>
2023-05-21 02:02:45 +0530

    testing

--- project_01/public/index.js
+++ project_01/public/index.js
@@ -1,44 +1,46 @@
-// This is the Node server which will handle my socket.io connection
+// // This is the Node server which will handle my socket.io connection
 
-const io = require('socket.io')(8000)
+// const io = require('socket.io')(8000)
 
-const users = {};
+// const users = {};
 
-const time = () => {
-    const date = new Date();
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    let meridiem = "am";
-    if(hours > 12){
-        hours = hours - 12;
-        meridiem = "pm"
-    }
-    else if (hours === 0){
-        hours = 12;
-        meridiem = am;
-    }
-    if(minutes <= 9){
-        minutes = `0${minutes}`
-    }
-    let time = `${hours}:${minutes} ${meridiem}`
-    return time;
-}
+// const time = () => {
+//     const date = new Date();
+//     let hours = date.getHours();
+//     let minutes = date.getMinutes();
+//     let meridiem = "am";
+//     if(hours > 12){
+//         hours = hours - 12;
+//         meridiem = "pm"
+//     }
+//     else if (hours === 0){
+//         hours = 12;
+//         meridiem = am;
+//     }
+//     if(minutes <= 9){
+//         minutes = `0${minutes}`
+//     }
+//     let time = `${hours}:${minutes} ${meridiem}`
+//     return time;
+// }
 
 
-io.on('connection', socket => {
-    socket.on('new-user-joined', name => {
-        // console.log("New user ", name)
-        users[socket.id] = name;
-        socket.broadcast.emit('user-joined', name);
-    });
+// io.on('connection', socket => {
+//     socket.on('new-user-joined', name => {
+//         // console.log("New user ", name)
+//         users[socket.id] = name;
+//         socket.broadcast.emit('user-joined', name);
+//     });
 
-    socket.on('send', message => {
-        socket.broadcast.emit('receive', {message: message, name: users[socket.id],time: time()})
-    });
+//     socket.on('send', message => {
+//         socket.broadcast.emit('receive', {message: message, name: users[socket.id],time: time()})
+//     });
 
-    socket.on('disconnect', () => {
-        socket.broadcast.emit('left', users[socket.id])
-        delete users[socket.id];
-    });
+//     socket.on('disconnect', () => {
+//         socket.broadcast.emit('left', users[socket.id])
+//         delete users[socket.id];
+//     });
 
-})
+// })
+
+console.log("index file working");
